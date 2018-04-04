ee54114389530f7b65cf38412b49283e3c3f0f52
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 03:40:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,10 +21,10 @@ end
 
 
 def now_serving(katz_deli)
-  if katz_deli.length > 0
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
-  else
-    "There is nobody waiting to be served!"
   end
 end

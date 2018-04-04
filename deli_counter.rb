3f7963f3274297fa8ab1e9741dbf0ea0f723c8d5
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 02:02:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,11 @@
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
+  else
+    cheque = []
+    katz_deli.each do |in_line|
+      cheque.push (katz_deli.index)
+    end
+    puts "The line is currently : #{array.index} #{cheque}".
   end
 end

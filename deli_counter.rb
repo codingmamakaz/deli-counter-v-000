3504447dcd94b8c61e29f91ed0aa6c32980befb7
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 03:33:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,13 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end

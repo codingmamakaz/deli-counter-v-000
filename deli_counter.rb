987f0de0153f93be6f71efade8227e7a6c421732
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 02:03:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,6 @@ def line(katz_deli)
     cheque = []
     katz_deli.each do |in_line|
       cheque.push (katz_deli.index)
-    end
     puts "The line is currently : #{array.index} #{cheque}".
   end
 end

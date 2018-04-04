f77f8eb40659de200d05368a30848de4a6443407
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 03:18:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,8 @@ def line(katz_deli)
     puts "The line is currently: #{cheque.join}"
   end
 end
+
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}.  You are number #{katz_deli.index(name) + 1} in line."
+end

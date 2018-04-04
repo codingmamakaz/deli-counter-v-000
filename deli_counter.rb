65182e5c353d1cfa851b1953110ee584e539d2bc
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-04 03:26:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,6 @@ end
 
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
